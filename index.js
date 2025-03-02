034eb224c4df875acaaf729e11ed635949898734
Nenad Vukmirovic <nenad@Nenads-iMac.local>
2025-03-02 18:44:35 +0100

    Commit Practicum

--- index.js
+++ index.js
@@ -8,11 +8,33 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
+const PRIVATE_APP_ACCESS = 'pat-eu1-a3f973fe-4c44-490f-b053-ae3067760bd6';
 
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
-// * Code for Route 1 goes here
+app.get("/", async (req, res) => {
+    const url = "https://api.hubspot.com/crm/v3/objects/concerts?properties=name,composer,concert_date_time";
+
+
+    const headers = {
+        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+        "Content-Type": "application/json",
+    };
+
+    const params = {
+        properties: ["name", "composer", "concert_date_time"],
+    };
+
+    try {
+        const getConcertsResponse = await axios.get(url, { headers, params });
+        console.log("API Response:", JSON.stringify(getConcertsResponse.data, null, 2));
+        const concertsData = getConcertsResponse.data.results;
+        console.log("Data:", JSON.stringify(concertsData, null, 2));
+        res.render("index", { concertsData });
+    } catch (error) {
+        console.error(error);
+    }
+});
 
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
